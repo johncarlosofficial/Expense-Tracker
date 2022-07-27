68d84474311826656366d017f9cb918ba9f92840
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-07-27 14:23:58 -0300

    Transaction Cumulative Sum

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -9,7 +9,9 @@
     import SwiftUICharts
 
     struct ContentView: View {
-        var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+        @EnvironmentObject var transactionListVM: TransactionListViewModel
+//        var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+        
         var body: some View {
             NavigationView {
                 ScrollView {
@@ -20,19 +22,24 @@
                             .bold()
                         
                         // MARK: Line Chart
-                        CardView {
-                            VStack {
-                            ChartLabel("$900", type: .title)
-
-                            LineChart()
-                            }
-                                .background(Color.systemBackground)
-                                
-                        }
-                        .data(demoData)
-                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
-                        .frame(height: 300)
+                        let data = transactionListVM.accumulateTransactions()
                         
+                        if !data.isEmpty {
+                            let totalExpenses = data.last?.1 ?? 0
+                            
+                            CardView {
+                                VStack(alignment: .leading) {
+                                    ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+
+                                LineChart()
+                                }
+                                    .background(Color.systemBackground)
+                                    
+                            }
+                            .data(data)
+                            .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                            .frame(height: 300)
+                        }
                         
                         // MARK: Transaction List
                         RecentTransactionList()
