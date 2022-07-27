4c653fe9018d9861ef5a9e67b9e42f4748df9160
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-07-27 13:51:11 -0300

    Line Chart

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -1,56 +1,74 @@
-//
-//  ContentView.swift
-//  ExpenseTracker
-//
-//  Created by João Carlos Magalhães on 12/07/22.
-//
+    //
+    //  ContentView.swift
+    //  ExpenseTracker
+    //
+    //  Created by João Carlos Magalhães on 12/07/22.
+    //
 
-import SwiftUI
+    import SwiftUI
+    import SwiftUICharts
 
-struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 24) {
-                    // MARK: Tittle
-                    Text("Overview")
-                        .font(.title2)
-                        .bold()
-                    // MARK: Transaction List
-                    RecentTransactionList()
+    struct ContentView: View {
+        var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+        var body: some View {
+            NavigationView {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 24) {
+                        // MARK: Tittle
+                        Text("Overview")
+                            .font(.title2)
+                            .bold()
+                        
+                        // MARK: Line Chart
+                        CardView {
+                            VStack {
+                            ChartLabel("$900", type: .title)
+
+                            LineChart()
+                            }
+                                .background(Color.systemBackground)
+                                
+                        }
+                        .data(demoData)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                        .frame(height: 300)
+                        
+                        
+                        // MARK: Transaction List
+                        RecentTransactionList()
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
                 }
-                .padding()
-                .frame(maxWidth: .infinity)
-            }
-            .background(Color.background)
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                // MARK: Notification Icon
-                ToolbarItem {
-                    Image(systemName: "bell.badge")
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(Color.icon, .primary)
+                .background(Color.background)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    // MARK: Notification Icon
+                    ToolbarItem {
+                        Image(systemName: "bell.badge")
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(Color.icon, .primary)
+                    }
                 }
             }
+            .navigationViewStyle(.stack)
+            .accentColor(.primary)
         }
-        .navigationViewStyle(.stack)
-        .accentColor(.primary)
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static let transactionListVM: TransactionListViewModel = {
-        let transactionListVM = TransactionListViewModel()
-        transactionListVM.transactions = transactionListPreviewData
-        return transactionListVM
-    }()
+    struct ContentView_Previews: PreviewProvider {
+        static let transactionListVM: TransactionListViewModel = {
+            let transactionListVM = TransactionListViewModel()
+            transactionListVM.transactions = transactionListPreviewData
+            return transactionListVM
+        }()
 
-    static var previews: some View {
-        Group {
-            ContentView()
-            ContentView()
-                .preferredColorScheme(.dark)
+        static var previews: some View {
+            Group {
+                ContentView()
+                ContentView()
+                    .preferredColorScheme(.dark)
+            }
+            .environmentObject(transactionListVM)
         }
-        .environmentObject(transactionListVM)
     }
-}
